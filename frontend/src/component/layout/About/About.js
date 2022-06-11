0d4fe36b31104fbe7e435b03b9600de4152dd81e
Pankaj Ahakey <ahakeypankaj@gmail.com>
2022-06-11 15:57:50 +0530

    first commit

---
+++ frontend/src/component/layout/About/About.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "./aboutSection.css";
+import { Button, Typography, Avatar } from "@material-ui/core";
+import InstagramIcon from "@material-ui/icons/Instagram";
+const About = () => {
+  const visitInstagram = () => {
+    window.location = "https://instagram.com/pankaj_ahakey";
+  };
+  return (
+    <div className="aboutSection">
+      <div></div>
+      <div className="aboutSectionGradient"></div>
+      <div className="aboutSectionContainer">
+        <Typography component="h1">About Us</Typography>
+
+        <div>
+          <div>
+            <Avatar
+              style={{ width: "10vmax", height: "10vmax", margin: "2vmax 0" }}
+              src="https://res.cloudinary.com/pankajahakey/image/upload/v1654757702/ywapayywrwaaadubuzjl.jpg"
+              alt="Founder"
+            />
+            <Typography>Abhishek Singh</Typography>
+            <Button onClick={visitInstagram} color="primary">
+              Visit Instagram
+            </Button>
+            <span>
+              This is a sample wesbite made by @PankajAhakey. Only with the
+              purpose to MERN Stack Project
+            </span>
+          </div>
+          <div className="aboutSectionContainer2">
+            <Typography component="h2">Our Brands</Typography>
+
+            <a href="https://instagram.com/pankaj_ahakey" target="blank">
+              <InstagramIcon className="instagramSvgIcon" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
